9890f12f40733bca3e8f9e913a671fd14e8df398
pratik mittal <pratik.mittal@driscolls.com>
2021-04-11 18:41:29 +0530

    added reducers

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Header">React Trello Clone</div>
+        <div className="Header">My Trello</div>
 
         <Board />
       </div>
